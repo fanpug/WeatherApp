2b73b60d1f09784e634d14e280806c379b83db3c
Humberto Navarro <a320669@uach.mx>
2023-08-09 10:15:51 -0600

    Add loading state to the app and expo location dependency

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tabs from './src/components/Tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  }
+})
 
 export default App
