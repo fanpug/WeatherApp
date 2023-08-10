5c510d19f8842cff3ac92d66782cb8a3da74fa9d
Humberto Navarro <a320669@uach.mx>
2023-08-10 09:51:36 -0600

    Add react native env package, modify babel and app json configs to accept env vars, create useGetWeather custom hook

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tabs from './src/components/Tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { ActivityIndicator, View, StyleSheet } from 'react-native';
+import { useGetWeather } from './src/hooks/useGetWeather';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, error, weather] = useGetWeather();
 
   if (loading) {
     return (
       <View style={styles.container}>
+        {/* <Text>{`${weather} ${error} ${loading}`}</Text> */}
         <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
